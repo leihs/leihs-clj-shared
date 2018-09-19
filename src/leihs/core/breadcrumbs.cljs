a2922f32ac1d146804bfb0f1be485cff81f5cee4
Thomas Schank <DrTom@schank.ch>
2018-09-19 14:20:38 +0200

    Internalize breadcrumbs and icons

---
+++ src/leihs/core/breadcrumbs.cljs
@@ -0,0 +1,38 @@
+(ns leihs.core.breadcrumbs
+  (:refer-clojure :exclude [str keyword])
+  (:require
+    [leihs.core.core :refer [keyword str presence]]
+    [leihs.core.icons :as icons]
+    [leihs.core.paths :as paths :refer [path]]
+    [leihs.core.routing.front :as routing]
+    ))
+
+(defn li
+  ([k n]
+   (li k n {} {}))
+  ([handler-key inner route-params query-params]
+   (let [active? (= (-> @routing/state* :handler-key) handler-key)]
+     [:li.breadcrumb-item {:key handler-key :class (if active? "active" "")}
+      (if active?
+        [:span inner]
+        [:a {:href (path handler-key route-params query-params)} inner])])))
+
+(defn nav-component [left right]
+  [:div.row.nav-component.mt-3
+   [:nav.col-lg {:aria-label :breadcrumb :role :navigation}
+    (when (seq left)
+      [:ol.breadcrumb
+       (for [li left] li) ])]
+   [:nav.col-lg {:role :navigation}
+    (when (seq right)
+      [:ol.breadcrumb.leihs-nav-right
+       (for [li right] li)])]])
+
+(defn admin-li [] (li :admin [:span icons/admin " Admin "]))
+(defn me-user-li [] (li :me-user [:span icons/user " User "]))
+(defn borrow-li [] (li :borrow "Borrow"))
+(defn debug-li [] (li :debug "Debug"))
+(defn initial-admin-li [] (li :initial-admin "Initial-Admin"))
+(defn leihs-li [] (li :home [:span icons/home " Home "]))
+(defn lending-li [] (li :lending "Lending"))
+(defn procurement-li [] (li :procurement "Procurement"))
---
+++ src/leihs/core/icons.cljs
@@ -0,0 +1,29 @@
+(ns leihs.core.icons
+  (:refer-clojure :exclude [str keyword])
+  (:require
+    [leihs.core.core :refer [keyword str presence]]))
+
+(def abort [:i.fas.fa-times-circle])
+(def add [:i.fas.fa-plus-circle])
+(def admin [:i.fas.fa-wrench])
+<<<<<<< HEAD
+(def api-token [:i.fas.fa-coins])
+(def api-token [:i.fas.fa-key])
+(def api-tokens api-token)
+(def api-tokens api-token)
+=======
+(def api-token [:i.fas.fa-key])
+(def api-tokens api-token)
+>>>>>>> WIP Internalize breadcrumbs and icons
+(def delegation [:i.fas.fa-hands-helping])
+(def delegations delegation)
+(def delete [:i.fas.fa-times])
+(def edit [:i.fas.fa-edit])
+(def group [:i.fas.fa-users])
+(def groups [:i.fas.fa-users])
+(def home [:i.fas.fa-home])
+(def me-user [:i.fas.fas-user])
+(def sign-in [:i.fas.fa-sign-in-alt])
+(def sign-out [:i.fas.fa-sign-out-alt])
+(def user [:i.fas.fa-user])
+(def users [:i.fas.fa-user-friends])
