e7cf3af876b7578bd78aeb6bc93e0aeb03aa4b74
Matus Kmit <matus.kmit@zhdk.ch>
2018-11-02 14:27:59 +0100

    redirect upon sign out

--- src/leihs/core/sign_out/back.clj
+++ src/leihs/core/sign_out/back.clj
@@ -2,36 +2,39 @@
   (:refer-clojure :exclude [str keyword])
   (:require
     [leihs.core.core :refer [str keyword presence presence!]]
+    [leihs.core.paths :refer [path]]
     [leihs.core.sql :as sql]
     [leihs.core.constants :refer [USER_SESSION_COOKIE_NAME]]
-
     [clojure.java.jdbc :as jdbc]
-
     [logbug.catcher :as catcher]
     [logbug.debug :as debug]
-    )
+    [ring.util.response :refer [redirect]])
   (:import
-    [java.util UUID]
-    ))
+    [java.util UUID]))
 
+(defn- delete-user-session [tx id]
+  (jdbc/delete! tx :user_sessions ["id = ?" id]))
 
 (defn ring-handler [{tx :tx
                      {user-session-id :user_session_id} :authenticated-entity
                      :as request}]
-  (if-let [user-session-id (-> request
-                               :authenticated-entity
-                               :user_session_id)]
-    (do (jdbc/delete! tx :user_sessions ["id = ?" user-session-id])
-        {:status 200
-         :body {}
-         :cookies
-         {leihs.core.constants/USER_SESSION_COOKIE_NAME
-          {:value nil
-           :http-only true
-           :max-age -1
-           :path "/"}}})
-    {:status 422}))
-
+  (let [user-session-id (-> request :authenticated-entity :user_session_id)
+        content-type (:content-type request)]
+    (if (= content-type :json)
+      (if user-session-id
+        (do (delete-user-session tx user-session-id)
+            {:status 200
+             :body {}
+             :cookies
+             {leihs.core.constants/USER_SESSION_COOKIE_NAME
+              {:value nil
+               :http-only true
+               :max-age -1
+               :path "/"}}})
+        {:status 422})
+      (do (if user-session-id
+            (delete-user-session tx user-session-id))
+          (redirect (path :home))))))
 
 ;#### debug ###################################################################
 ;(logging-config/set-logger! :level :debug)
