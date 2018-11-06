1d194efe07b90bb89a752e3fd471b96eb6d63c6d
Matus Kmit <matus.kmit@zhdk.ch>
2018-11-06 13:35:29 +0100

    use accept mime type

--- src/leihs/core/sign_out/back.clj
+++ src/leihs/core/sign_out/back.clj
@@ -1,16 +1,17 @@
 (ns leihs.core.sign-out.back
   (:refer-clojure :exclude [str keyword])
-  (:require
-    [leihs.core.core :refer [str keyword presence presence!]]
-    [leihs.core.paths :refer [path]]
-    [leihs.core.sql :as sql]
-    [leihs.core.constants :refer [USER_SESSION_COOKIE_NAME]]
-    [clojure.java.jdbc :as jdbc]
-    [logbug.catcher :as catcher]
-    [logbug.debug :as debug]
-    [ring.util.response :refer [redirect]])
+  (:require [clojure.tools.logging :as log]
+
+            [leihs.core.core :refer [str keyword presence presence!]]
+            [leihs.core.paths :refer [path]]
+            [leihs.core.sql :as sql]
+            [leihs.core.constants :refer [USER_SESSION_COOKIE_NAME]]
+            [clojure.java.jdbc :as jdbc]
+            [logbug.catcher :as catcher]
+            [logbug.debug :as debug]
+            [ring.util.response :refer [redirect]])
   (:import
-    [java.util UUID]))
+   [java.util UUID]))
 
 (defn- delete-user-session [tx id]
   (jdbc/delete! tx :user_sessions ["id = ?" id]))
@@ -19,8 +20,8 @@
                      {user-session-id :user_session_id} :authenticated-entity
                      :as request}]
   (let [user-session-id (-> request :authenticated-entity :user_session_id)
-        content-type (:content-type request)]
-    (if (= content-type :json)
+        accept-type (-> request :accept :mime)]
+    (if (= accept-type :json)
       (if user-session-id
         (do (delete-user-session tx user-session-id)
             {:status 200
