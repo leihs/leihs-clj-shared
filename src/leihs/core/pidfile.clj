50713aedff758367b65eb8a596dd0ab7e27ead74
Matus Kmit <matus.kmit@zhdk.ch>
2019-09-26 11:34:45 +0200

    revert pidfile

--- src/leihs/core/pidfile.clj
+++
@@ -1,9 +0,0 @@
-(ns leihs.core.pidfile
-  (:require [clj-pid.core :as pid]
-            [clojure.tools.logging :as log]))
-
-(defn handle [file-path]
-  (.mkdirs (java.io.File. "./tmp"))
-  (pid/save file-path)
-  (pid/delete-on-shutdown! file-path)
-  (log/info "pidfile written to:" file-path))
