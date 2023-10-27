65fce7b9f8136f290ab1b4c530f6d4354e62279b
mradl <manuel.radl@zhdk.ch>
2023-10-27 13:36:13 +0200

    needed revision to run my/next-dev

--- src/leihs/core/auth/shared.clj
+++ src/leihs/core/auth/shared.clj
@@ -1,19 +1,31 @@
 (ns leihs.core.auth.shared
-  (:refer-clojure :exclude [str keyword])
+  (:refer-clojure :exclude [keyword str])
   (:require
     [clojure.java.jdbc :as jdbc]
-    [leihs.core.core :refer [str keyword presence presence!]]
+
+    [honey.sql :as sqlh]
+    [honey.sql.helpers :as h]
+    [next.jdbc :as jdbc-next]
+    [leihs.core.db :as db]
+
     [leihs.core.sql :as sql]
-    [logbug.catcher :as catcher]
-    [logbug.debug :as debug]
+
     ))
 
+;; TODO: cleanup
 (defn access-rights [tx user-id]
   (-> (sql/select :role :inventory_pool_id)
       (sql/from :access_rights)
       (sql/merge-where [:= :user_id user-id])
       sql/format
       (->> (jdbc/query tx))))
+
+(defn access-rights-new [tx user-id]                            ;;OK
+  (-> (h/select :role :inventory_pool_id)
+      (h/from :access_rights)
+      (h/where [:= :user_id user-id])
+      sqlh/format
+      (->> (jdbc-next/execute! tx))))
 
 (defn password-hash
   [password tx]
@@ -26,3 +38,25 @@
        (jdbc/query tx)
        first
        :pw_hash))
+
+(defn password-hash-new                                         ;;OK
+  ([password tx]
+   (->> ["SELECT crypt(?,gen_salt('bf',10)) AS pw_hash" password]
+        (jdbc-next/execute-one! tx)
+        :pw_hash)))
+
+(comment
+  (let [
+        tx-next (db/get-ds-next)
+        tx (db/get-ds)
+
+        ;res1 (access-rights-old tx #uuid "02563014-d74d-5572-a8b2-3736738459ba"  )
+        res1 (access-rights tx-next #uuid "02563014-d74d-5572-a8b2-3736738459ba")
+        p (println "\n\n>res1>> " res1 "\n\n")
+
+        ;[SELECT crypt(CAST(? AS text), gen_salt('bf', 10)) AS pw_hash  password]
+        ;res1 (password-hash "password" tx )
+
+        res1 (password-hash-new "password" tx-next)
+        p (println "\n\n>res1>> " res1 "\n\n")
+        ]))
