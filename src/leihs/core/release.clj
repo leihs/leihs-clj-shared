6e4886a315c791271c5ba58b70dde117bbd69737
Matus Kmit <matuskmit1@gmail.com>
2025-03-12 12:11:20 +0100

    fix: leihs version handling

--- src/leihs/core/release.clj
+++ src/leihs/core/release.clj
@@ -3,21 +3,14 @@
    [leihs.core.core :refer [presence]]
    [taoensso.timbre :refer [debug error info spy warn]]))
 
-(def VERSION-FILE-NAME "LEIHS-VERSION")
 (def DEV-VERSION "dev")
 (def BETA-SUFFIX "-beta")
 (def GH-URL "https://github.com/leihs/leihs")
 (def GH-RELEASES-URL (str GH-URL "/releases"))
 
-(def file-content
-  (some-> VERSION-FILE-NAME
-          clojure.java.io/resource
-          slurp
-          clojure.string/split-lines
-          first))
-
 (def version
-  (or (presence file-content) DEV-VERSION))
+  (or (presence (System/getenv "LEIHS_VERSION"))
+      DEV-VERSION))
 
 (def gh-link
   (if (or (= version DEV-VERSION)
