c2d07f10e44f46a4c747529fd18bc326b9730788
Matus Kmit <matuskmit1@gmail.com>
2026-02-24 10:15:27 +0100

    fix: json-protocol -write arity for data.json 2.x

--- src/leihs/core/json_protocol.clj
+++ src/leihs/core/json_protocol.clj
@@ -11,20 +11,20 @@
   (time-format/unparse (time-format/formatters :date-time) date-time))
 
 (clojure.core/extend-type org.joda.time.DateTime clojure.data.json/JSONWriter
-                          (-write [date-time out]
-                            (clojure.data.json/write (date-time-to-string date-time) out)))
+                          (-write [date-time out options]
+                            (clojure.data.json/write (date-time-to-string date-time) out options)))
 
 (clojure.core/extend-type java.sql.Timestamp clojure.data.json/JSONWriter
-                          (-write [sql-time out]
-                            (clojure.data.json/write (time-coerce/from-sql-time sql-time) out)))
+                          (-write [sql-time out options]
+                            (clojure.data.json/write (time-coerce/from-sql-time sql-time) out options)))
 
 (clojure.core/extend-type java.time.Instant clojure.data.json/JSONWriter
-                          (-write [inst out]
-                            (clojure.data.json/write (str inst) out)))
+                          (-write [inst out options]
+                            (clojure.data.json/write (str inst) out options)))
 
 (clojure.core/extend-type java.util.UUID clojure.data.json/JSONWriter
-                          (-write [uuid out]
-                            (clojure.data.json/write (. uuid toString) out)))
+                          (-write [uuid out options]
+                            (clojure.data.json/write (. uuid toString) out options)))
 
 (cheshire.generate/add-encoder org.joda.time.DateTime
                                (fn [date-time jsonGenerator]
