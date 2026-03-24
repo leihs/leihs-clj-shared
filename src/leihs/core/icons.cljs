46df20d0adc45a63c319d2f4bee2d04d3424c18c
uvb <urs.vanbinsbergen@zhdk.ch>
2026-03-24 16:01:17 +0100

    chore: update @fortawesome/react-fontawesome to 0.2.0

--- src/leihs/core/icons.cljs
+++ src/leihs/core/icons.cljs
@@ -1,14 +1,12 @@
 (ns leihs.core.icons
   (:refer-clojure :exclude [str keyword])
   (:require
-   ["@fortawesome/free-brands-svg-icons" :as brands]
    ["@fortawesome/free-solid-svg-icons" :as solids]
-   ["@fortawesome/react-fontawesome" :as fa-react-fontawesome :refer [FontAwesomeIcon]]
-   [leihs.core.core :refer [keyword str presence]]))
+   ["@fortawesome/react-fontawesome" :as fa-react-fontawesome :refer [FontAwesomeIcon]]))
 
-(defn admin [] (FontAwesomeIcon #js{:icon solids/faWrench :className ""}))
-(defn delete [] (FontAwesomeIcon #js{:icon solids/faTimes :className ""}))
-(defn home [] (FontAwesomeIcon #js{:icon solids/faHome :className ""}))
+(defn admin [] [:> FontAwesomeIcon {:icon solids/faWrench :className ""}])
+(defn delete [] [:> FontAwesomeIcon {:icon solids/faTimes :className ""}])
+(defn home [] [:> FontAwesomeIcon {:icon solids/faHome :className ""}])
 
 
 
