9fb2ff285589727b9ed126ba847be4740e2c1274
Matus Kmit <matuskmit1@gmail.com>
2026-05-06 14:28:15 +0200

    chore: commit sha version link logic

--- src/leihs/core/release.clj
+++ src/leihs/core/release.clj
@@ -1,10 +1,11 @@
 (ns leihs.core.release
   (:require
+   [clj-yaml.core :as yaml]
+   [clojure.java.io :as io]
    [leihs.core.core :refer [presence]]
    [taoensso.timbre :refer [debug error info spy warn]]))
 
 (def DEV-VERSION "dev")
-(def BETA-SUFFIX "-beta")
 (def GH-URL "https://github.com/leihs/leihs")
 (def GH-RELEASES-URL (str GH-URL "/releases"))
 
@@ -13,7 +14,22 @@
       DEV-VERSION))
 
 (def gh-link
-  (if (or (= version DEV-VERSION)
-          (clojure.string/ends-with? version BETA-SUFFIX))
+  (if (= version DEV-VERSION)
     GH-URL
     (str GH-RELEASES-URL "/tag/" version)))
+
+(def built-info
+  (some-> "built-info.yml" io/resource slurp yaml/parse-string))
+
+(def super-commit-id
+  (:super_commit_id built-info))
+
+(def display-version
+  (if (and (= version DEV-VERSION) super-commit-id)
+    (subs super-commit-id 0 7)
+    version))
+
+(def display-gh-link
+  (if (and (= version DEV-VERSION) super-commit-id)
+    (str GH-URL "/tree/" super-commit-id)
+    gh-link))
